e8bed117ac2a7b86f8044356f41b9a6cc9d4bc7e
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-29 20:04:31 +0700

    feat: описать типы в схеме, параметры для мутации, тип запроса, создать resolver

--- server/app.js
+++ server/app.js
@@ -2,11 +2,29 @@ const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors')
 const PORT = 3001
-
+const schema = require('./schema')
 const app = express()
+const allUsers = [{
+    id: 1,
+    username: "Denis",
+    age: 28
+}]
+
 app.use(cors())
+
+const root = {
+    getAllUsers: () => {
+        return allUsers
+    },
+    getUser: ({id}) => {
+        return allUsers.find(user => user.id == id)
+    }
+}
+
 app.use('/graphql', graphqlHTTP({
-    graphiql: true
+    graphiql: true,
+    schema,
+    rootValue: root
 }))
 
 app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
