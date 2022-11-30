1aabec9f7ca37f01ceec9f5e156bc18b3f43e702
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-30 20:45:35 +0700

    feat: установить ant design

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Input, Form, Button, Row, Col } from "antd";
 
 function App() {
-    return (
-        <div className="App">
-        </div>
-    );
+  return (
+    <div className="App">
+      <Form>
+        <Row justify="center">
+          <Col xs={6}>
+            <Input></Input>
+          </Col>
+        </Row>
+        <Row justify="center">
+          <Col xs={6}>
+            <Input></Input>
+          </Col>
+        </Row>
+        <Row justify="center">
+          <Col xs={3}>
+            <Button type="primary">Создать пользователя</Button>
+          </Col>
+          <Col xs={3}>
+            <Button type="primary">Получить пользователей</Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
 }
 
 export default App;
