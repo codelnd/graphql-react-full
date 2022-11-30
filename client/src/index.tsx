7c1c86f27f5a1cdac99ff60c72fdb18c8bdb9150
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-30 20:12:04 +0700

    feat: установить apollo, graphql, импорт провайдера и клиента

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-    <App />
-);
+root.render(<App />);
