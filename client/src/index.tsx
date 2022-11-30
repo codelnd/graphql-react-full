4d8edd0ed730f459d6d2bf935afd56ea71b2297e
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-30 20:20:34 +0700

    feat: создать client и обернуть в apollo provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: "http://localhost:3001/graphql",
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
