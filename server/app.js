84dcdd871faeeb20de0bbe8354baecdc78a4b246
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-01 19:10:36 +0700

    refactor: сменить тип схемы, подключить ее через модуль fs

--- server/app.js
+++ server/app.js
@@ -1,43 +1,51 @@
-const express = require('express')
-const {graphqlHTTP} = require('express-graphql')
-const cors = require('cors')
-const PORT = 3001
-const schema = require('./schema')
-const app = express()
-const allUsers = [{
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const PORT = 3001;
+const { buildSchema } = require("graphql");
+const { readFileSync } = require("fs");
+const schemaString = readFileSync("./schema.graphql", { encoding: "utf-8" });
+const schema = buildSchema(schemaString);
+const app = express();
+const allUsers = [
+  {
     id: 1,
     username: "Denis",
-    age: 28
-}]
+    age: 28,
+  },
+];
 
-app.use(cors())
-
+app.use(cors());
 
 const createUser = (input) => {
-    const id = Date.now()
-    return {
-        id, ...input
-    }
-}
+  const id = Date.now();
+  return {
+    id,
+    ...input,
+  };
+};
 
 const root = {
-    getAllUsers: () => {
-        return allUsers
-    },
-    getUser: ({id}) => {
-        return allUsers.find(user => user.id == id)
-    },
-    createUser: ({input}) => {
-        const user = createUser(input)
-        allUsers.push(user)
-        return user
-    }
-}
+  getAllUsers: () => {
+    return allUsers;
+  },
+  getUser: ({ id }) => {
+    return allUsers.find((user) => user.id == id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    allUsers.push(user);
+    return user;
+  },
+};
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     graphiql: true,
     schema,
-    rootValue: root
-}))
+    rootValue: root,
+  })
+);
 
-app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
+app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`));
